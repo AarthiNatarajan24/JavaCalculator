bfe235cb3472286e2528996c1820ec1a51318e35
AarthiNataraj24 <natrajanrn27@gmail.com>
2026-09-13 19:58:04 +0530

    added calci files for ci/cd

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(3, calculator.subtract(5, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5, calculator.divide(10, 2));
+    }
+}
